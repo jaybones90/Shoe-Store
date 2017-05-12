7421384088e1532dfbed6bc93f4de8fa8f294335
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-05-12 12:39:52 -0700

    add in passing test for callback method to capitalize the first letter of the shoe brand

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,13 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:brand, :presence => true)
+  before_save(:capitalize_first_letter)
+
+  private
+
+  def capitalize_first_letter
+    self.brand = brand.capitalize!
+  end
+
+
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -4,10 +4,17 @@ describe(Shoe) do
   describe('#stores') do
     it {should have_and_belong_to_many(:stores)}
   end
-  it('validates the presence of a name') do
+  it('validates the presence of a brand') do
     test_shoe = Shoe.new({:brand => ""})
     expect(test_shoe.save).to(eq(false))
   end
-
+  it('validates the presence of a price') do
+    test_shoe = Shoe.new({:price => ""})
+    expect(test_shoe.save).to(eq(false))
+  end
+  it("makes the first letter of the brand capitalized") do
+    test_shoe = Shoe.create({:brand => "nike"})
+    expect(test_shoe.brand).to(eq("Nike"))
+  end
 
 end
